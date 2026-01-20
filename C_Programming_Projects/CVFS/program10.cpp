ac5630ad4ec512fa8ce62caa1a25865dfaf29e6b
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-01-20 11:23:11 +0530

    Add one variable named as FileType

---
+++ C_Programming_Projects/CVFS/program10.cpp
@@ -0,0 +1,388 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Header File Inclusion
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>    
+#include<stdlib.h>    
+#include<unistd.h>    
+#include<stdbool.h>  
+#include<string.h>   
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  User Defined Macros
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Maximum file size that we allow in the project
+#define MAXFILESIZE 50
+
+#define MAXOPENFILES 20
+
+#define MAXINODE 5
+
+#define READ 1
+#define WRITE 2
+#define EXECUTE 4
+
+#define SATRT 0
+#define CURRENT 1
+#define END 2
+
+#define REGULARFILE 1
+#define SPECIALFILE 2
+
+#define EXECUTE_SUCCESS 0
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  User Defined Structures
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Name : BootBlock
+//  Description    : Holds the information to boot the OS
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct BootBlock
+{
+    char Information[100];
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Name : SuperBlock
+//  Description    : Holds the information about the file system
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct SuperBlock
+{
+    int TotalInodes;
+    int FreeInodes;
+};
+                                                                                                                                                                        
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Name : Inode
+//  Description    : Holds the information about the file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma pack(1)
+struct Inode
+{
+    char FileName[20];
+    int InodeNumber;
+    int FileSize;
+    int ActualFileSize;
+    int FileType;
+    int ReferenceCount;
+    int Permission;
+    char *Buffer;             // Data Block
+    struct Inode *next;
+};
+
+typedef struct Inode INODE;
+typedef struct Inode * PINODE;
+typedef struct Inode ** PPINODE;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Name : FileTable
+//  Description    : Holds the information about opened file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct FileTable
+{
+    int ReadOffset;     // From where to read
+    int WriteOffset;    // from where to write
+    int Mode;           // file open in which mode or which permission is give to open file
+    PINODE ptrinode;
+};
+
+typedef FileTable FILETABLE;
+typedef FileTable * PFILETABLE;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Name : UAREA
+//  Description    : Holds the information about process
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct UAREA
+{
+    char ProcessName[20];
+    PFILETABLE UFDT[MAXOPENFILES];
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Global variable or objects used in the project
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+BootBlock bootobj;
+SuperBlock superobj;
+UAREA uareaobj;
+
+PINODE head = NULL;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     InitializeUAREA
+//  Description :       It is used to initialize UAREA members
+//  Author :            Sahil Rajaram Thorat
+//  Date :              13/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InitializeUAREA()
+{
+    strcpy(uareaobj.ProcessName, "Myexe");
+
+    int i = 0;
+
+    for(i = 0; i < MAXOPENFILES; i++)
+    {
+        uareaobj.UFDT[i] = NULL;
+    }
+
+    printf("Marvellous CVFS : UAREA gets initialized successfully\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     InitializeSuperBlock
+//  Description :       It is used to initialize Super block members
+//  Author :            Sahil Rajaram Thorat
+//  Date :              13/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InitializeSuperBlock()
+{
+    superobj.TotalInodes = MAXINODE;
+    superobj.FreeInodes = MAXINODE;
+
+    printf("Marvellous CVFS : Super block gets initialized successfully\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     CreateDILB
+//  Description :       It is used to create Linkedlist of Inodes
+//  Author :            Sahil Rajaram Thorat
+//  Date :              13/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CreateDILB()
+{
+    int i = 1;
+    PINODE newn = NULL;
+    PINODE temp = head;
+
+    for(i = 1; i <= MAXINODE; i++)
+    {
+        newn = (PINODE)malloc(sizeof(INODE));
+
+        strcpy(newn -> FileName ,"\0");
+        newn -> InodeNumber = i;
+        newn -> FileSize = 0;
+        newn -> ActualFileSize = 0;
+        newn -> FileType = 0;
+        newn -> ReferenceCount = 0;
+        newn -> Permission = 0;
+        newn -> Buffer = NULL;
+        newn -> next = NULL;
+
+        if(temp == NULL)                        // LL is empty
+        {
+            head = newn;
+            temp = head;
+        }
+        else                                    // LL contains atleast one node
+        {
+            temp -> next = newn;
+            temp = temp -> next;            
+        }
+    }
+
+    printf("Marvellous CVFS : DILB created successfully\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     StartAuxillaryDataInitialization
+//  Description :       It is used to all such functions which are used to initialize auxyilary data.
+//  Author :            Sahil Rajaram Thorat
+//  Date :              13/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void StartAuxillaryDataInitialization()
+{
+    strcpy(bootobj.Information, "Booting process of Marvellous CVFS is done");
+
+    printf("%s\n", bootobj.Information);
+
+    InitializeSuperBlock();
+
+    CreateDILB();
+
+    InitializeUAREA();
+
+    printf("Marvellous CVFS : Auxillary data initialized successfully\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     DisplayHelp
+//  Description :       It is used to diplay the help page.
+//  Author :            Sahil Rajaram Thorat
+//  Date :              14/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void DisplayHelp()
+{
+    printf("\n-----------------------------------------------------------------------------\n");
+    printf("------------------------- Marvellous CVFS Help Page -------------------------\n");
+    printf("-----------------------------------------------------------------------------\n");
+
+    printf("man    : It is used to display manual page\n");
+    printf("clear  : It is used to clear the terminal\n");
+    printf("creat  : It is used to create new file\n");
+    printf("write  : It is used to write the data into the file\n");
+    printf("read   : It is used to read the data from the file\n");
+    printf("stat   : It is used to display statistical information\n");
+    printf("unlink : It is used to delete the file\n");
+    printf("exit   : It is used to terminate Marvellous CVFS\n");
+
+    printf("-----------------------------------------------------------------------------\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :     ManPageDisplay
+//  Description :       It is used to diplay Man page.
+//  Author :            Sahil Rajaram Thorat
+//  Date :              14/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void ManPageDisplay(char Name[])
+{
+    if(strcmp("ls", Name) == 0)
+    {
+        printf("\nAbout : It is used to list the names of all files\n");
+        printf("Usage : ls\n");
+    }
+    else if(strcmp("man", Name) == 0)
+    {
+        printf("\nAbout : It is used to display manual page\n");
+        printf("Usage : man Command_name\n");
+        printf("Command_name : It is the name of command\n");
+    }
+    else if(strcmp("exit", Name) == 0)
+    {
+        printf("\nAbout : It is used to terminate the shell\n");
+        printf("Usage : exit\n");
+    }
+    else if(strcmp("clear", Name) == 0)
+    {
+        printf("\nAbout : It is used to clear the shell\n");
+        printf("Usage : clear\n");
+    }
+    else
+    {
+        printf("No manual entry for %s\n", Name);
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function of the project   
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char str[80] = {'\0'};
+    char Command[5][20] = {{'\0'}};
+    int iCount = 0;
+
+    StartAuxillaryDataInitialization();
+
+    printf("\n-----------------------------------------------------------------------------\n");
+    printf("--------------------- Marvellous CVFS started sucessfully -------------------\n");
+    printf("-----------------------------------------------------------------------------\n");
+
+    // Infinite Listening Shell
+    while(1)
+    {
+        fflush(stdin);
+
+        strcpy(str,"");
+
+        printf("\nMarvellous CVFS : > ");
+        fgets(str, sizeof(str), stdin);        // Scanf("%s",str);
+
+        iCount = sscanf(str,"%s %s %s %s %s",Command[0],Command[1],Command[2],Command[3],Command[4]);
+
+        fflush(stdin);
+
+        if(iCount == 1)
+        {
+            if(strcmp("exit",Command[0]) == 0)
+            {
+                printf("Thank you for using Marvellous CVFS\n");
+                printf("Deallocating all the allocated resources\n");
+
+                break;
+            }
+            else if(strcmp("ls",Command[0]) == 0)
+            {
+                printf("Inside ls\n");
+            }
+            else if(strcmp("help",Command[0]) == 0)
+            {
+                DisplayHelp();
+            }
+            else if(strcmp("clear",Command[0]) == 0)
+            {
+                #ifdef _WIN32        // If windows
+                    system("cls");
+                #else                // If linux/macOS
+                    system("clear")
+                #endif
+            }
+        } // End of if 1
+        else if(iCount == 2)
+        {
+            if(strcmp("man",Command[0]) == 0)
+            {
+                ManPageDisplay(Command[1]);
+            }
+        } // End of else if 2
+        else if(iCount == 3)
+        {} // End of else if 3
+        else if(iCount == 4)
+        {} // End of else if 4
+        else
+        {
+            printf("Command not found\n");
+            printf("Please refer help option to get more information\n");
+        } // End of else
+    } // End of while
+
+    return 0;
+} // End of main
