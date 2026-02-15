5129bfc7dfd7518c2afd993a6ead4b09b8616533
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-15 20:16:32 +0530

    cdx command to change current directory like linux cd command

---
+++ C_Programming_Projects/Command_Suite/Command_cd.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+//      ./cdx        DirectoryPath    
+//      argv[0]      argv[1]         
+//      argc = 2
+
+/* 
+        Accept new directory path
+        Call chdir with that path
+        call getcwd to check the changed path
+*/
+
+#define MAX_BUFFER_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        printf("Error : Inssufficient arguments\n");
+        printf("Use command as : ./cdx DirectoryPath\n");
+        return -1;
+    }
+
+    if(chdir(argv[1]) == -1)
+    {
+        printf("Error : Unable to change current directory\n");
+
+        return -1;
+    }
+
+    char Buffer[256] = {'\0'};
+
+    getcwd(Buffer, sizeof(Buffer) - 1);
+
+    printf("Current directory changed to %s\n", Buffer);
+
+    return 0;
+}
