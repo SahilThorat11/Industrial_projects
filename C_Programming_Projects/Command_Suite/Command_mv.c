35f2856d5e7c97b662f9f5fc27f059acd8c86cf3
SahilThorat11 <sahilrajaramthorat11@gmail.com>
2026-02-15 20:18:43 +0530

    mvx command like linux mv command

---
+++ C_Programming_Projects/Command_Suite/Command_mv.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+//      ./mvx        SourcePath      DestPath
+//      argv[0]      argv[1]         argv[2]
+
+//      argc = 3
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        printf("Error : Inssufficient arguments\n");
+        printf("Use command as : ./mvx SourcePath DestinationPath\n");
+        return -1;
+    }
+
+    if(rename(argv[1], argv[2]) == -1)
+    {
+        printf("Error : Unable to move\n");
+        return -1;
+    }
+
+    return 0;
+}
